286d2f8794551419b581bfa79f4b186fe9b9026d
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2019-09-18 13:32:26 -0400

    turned off pin memory for timing experiment

--- datasets/procon.py
+++ datasets/procon.py
@@ -29,17 +29,17 @@ class ProConDataLoader:
 		val_idxs = self.folds[fold_num]
 		val_sampler = SubsetRandomSampler(val_idxs)
 		val_loader = DataLoader(self.train_set, self.config.batch_size, 
-						num_workers=self.config.num_workers, sampler=val_sampler, pin_memory=True)
+						num_workers=self.config.num_workers, sampler=val_sampler)
 		train_idxs = [idx for i,fold in enumerate(self.folds) if i!=fold_num for idx in fold]
 		train_sampler = SubsetRandomSampler(train_idxs)
 		train_loader = DataLoader(self.train_set, self.config.batch_size, 
-						num_workers=self.config.num_workers, sampler=train_sampler, pin_memory=True)
+						num_workers=self.config.num_workers, sampler=train_sampler)
 		return train_loader, val_loader
 
 	def getTrainLoader(self):
 		return DataLoader(self.train_set, self.config.batch_size, 
-				num_workers=self.config.num_workers, pin_memory=True)
+				num_workers=self.config.num_workers)
 
 	def getTestLoader(self):
 		return DataLoader(self.test_set, self.config.batch_size, 
-				num_workers=self.config.num_workers, pin_memory=True)
+				num_workers=self.config.num_workers)
